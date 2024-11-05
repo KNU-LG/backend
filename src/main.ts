15610ac8c1a0a7cb9da5c80558a03905de7f9be1
JongunJeong <skeep194@knu.ac.kr>
2024-11-05 18:53:07 +0900

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   const configService: ConfigService<EnvironmentVariables> =
     app.get(ConfigService);
 
